9edb5a860b05caf03a4534588082f53ce40188eb
naeemmia2021 <82281513+naeemmia2021@users.noreply.github.com>
2022-03-28 00:50:09 +0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,155 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Naeem's CV </title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<script src='https://kit.fontawesome.com/a076d05399.js' crossorigin='anonymous'></script>
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	<style type="text/css">
+		#div1{
+			width: 25%;
+			float: left;
+			padding-left: 10px;
+			
+		}
+		#div2{
+			width: 70%;
+			float: right;
+		}
+		img{
+			border-radius: 50%;
+		}
+		#innerdiv2{
+			height: 150px;
+  			width: 100%;
+  			background-color: 	#bf4040;
+  			color: white;
+		}
+		hr.c1{
+			    border: 2px solid white;
+  				border-radius: 5px;
+		}
+		#nameid{
+			padding: 10px 0px 0px 10px;
+		}
+		.designationid{
+			padding-left: 10px;
+		}
+		.container {
+ 			 width: 100%;
+  			 background-color: #ddd;
+		}
+
+		.skills {
+  			 text-align: right;
+  			 padding-top: 5px;
+  			 padding-bottom: 5px;
+  			 color: white;
+		}
+
+		.html {width: 90%; background-color: #04AA6D;}
+		.css {width: 80%; background-color:  #2196F3;}
+		.js {width: 65%; background-color:   #f44336;}
+		.php {width: 60%; background-color:  #808080;}
+		.java{width: 60%; background-color:  #3380cc;}
+		.android{width: 60%; background-color:  #dbadeb ;}
+		.sql{width: 70%;background-color: #66ccff;}
+	</style>
+</head>
+<body>
+<div id="div1" class="d1"> 
+   <img src="naeem.jpg" height="250px" width="250px">
+
+   <h2>ABOUT ME</h2>
+   <hr>
+   <p>Date of birth: january 22, 1999</p>
+   	 <p> Age: 23</p>
+   	<p>  Gender: Male</p>
+   	<p>  Birth Place: Monohardi, Narsingdi</p>
+   <br>
+   <br>
+   <h2>CONTACT</h2>
+   <hr>
+   <i class='fas fa-map-marker-alt' style='font-size:24px'></i>  Dhaka, Bangladesh
+   <p><i style='font-size:24px' class='fas'>&#xf095;</i>    01791796640</p>
+   <p><i style="font-size:24px" class="fa">&#xf0e0;</i>  mia1707041@stud.kuet.ac.bd</p>
+   <br>
+   <br>
+   <h2>SKILLS</h2>
+   <hr>
+
+   <p>HTML</p>
+  <div class="container">
+  <div class="skills html">90%</div>
+ </div>
+ <p>CSS</p>
+<div class="container">
+  <div class="skills css">80%</div>
+</div>
+<p>JavaScript</p>
+<div class="container">
+  <div class="skills js">65%</div>
+</div>
+<p>PHP</p>
+<div class="container">
+  <div class="skills php">60%</div>
+</div>
+<p>SQL</p>
+<div class="container">
+  <div class="skills sql">70%</div>
+</div>
+<p>JAVA</p>
+<div class="container">
+  <div class="skills java">60%</div>
+</div>
+<p>ANDROID</p>
+<div class="container">
+  <div class="skills android">60%</div>
+</div>
+
+ </div>
+<div id="div2"> 
+	<div id="innerdiv2">
+		<h1 id="nameid">Naeem Mia</h1>
+		<h3 class="designationid">Junior software developer</h3>
+	</div>
+	<h2>CARRIER OBJECTIVES</h2>
+	<hr>
+	<h3><p class="designationid">I have a courage to learn something new. A software engineering graduate who is seeking to find the oppurtunity to work in a fun and challenging work environment that will courage me to improve and learn new and necessary skills as well as to motivate by the company to do the best for the sake of helping myself and the company advance in the software engineering industry. </p> </h3>
+
+	<h2>ACADEMIC BACKGROUND</h2>
+	<hr>
+	<h3>2018-2023  Bachelor Degree</h3>
+	<p> B.Sc. in computer science</p>
+	  <p>  Khulna University of Engineering & Technology
+	</p>
+
+	<h2>INTERNSHIPS</h2>
+	<hr>
+	<h3>2022 - B K firm</h3>
+	<p> Dhaka, Bangladesh</p>
+	<h3>2023 - BJIT</h3>
+	<p> Dhaka, Bangladesh</p>
+
+	<h2>REFERENCES</h2>
+	<hr>
+	<h3 class="#designationid">Dr. Sk. Md. Masudul Ahsan</h3>
+	<p>	Professor</p>
+	<p>	Department of Computer Science & Engineering</p>
+	<p>	Khulna University of Engineering & Technology (KUET)</p>
+	<p>	Khulna -9203, Bangladesh.</p>
+	<p>	Mail : smahsan@cse.kuet.ac.bd</p>
+	<p>	Website : www.kuet.ac.bd/cse/smmasudul/</p>
+	<br>
+	<br>
+	<h3>Dr. Kazi Md. Rokibul Alam</h3>
+	<p>	Professor</p>
+	<p>	Department of Computer Science and Engineering (CSE)</p>
+	<p>	Khulna University of Engineering & Technology (KUET)</p>
+	<p>	Khulna -9203, Bangladesh. Phone: +880 41-769471-365</p>
+	<p>	Cell: +880 1714 087 216, Fax: +880 41 774318, Mail : rokib@cse.kuet.ac.bd</p>
+	<p>	Website : www.kuet.ac.bd/cse/rokib/</p>
+ </div>
+</body>
+</html>
